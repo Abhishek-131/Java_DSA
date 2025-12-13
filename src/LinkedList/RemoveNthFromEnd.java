07c98c91bcf4355aa4a2bdd7c35d9fde13ccc3cd
Abhishek Kumar <abhisheksdot@gmail.com>
2025-12-13 23:05:12 +0530

    Remove Nth From End 2nd Time

--- src/LinkedList/RemoveNthFromEnd.java
+++ src/LinkedList/RemoveNthFromEnd.java
@@ -2,29 +2,29 @@ package LinkedList;
 
 public class RemoveNthFromEnd {
     //Brute Force
-    private static Node RemoveNthNodeFronEnd(Node head, int n){
-        int cnt = 0;
-        Node temp = head;
-        //First Count the Size of the
-        while (temp != null){
-            cnt++;
-            temp= temp.next;
-        }
-        //Edge case if the count == N then change the head
-        if(cnt == n){
-            Node newNode = head.next;
-            return newNode;
-        }
-        int res = cnt - n;
-        temp = head;
-        while (temp != null){
-           res--;
-           if(res == 0) break;
-            temp = temp.next;
-        }
-        temp.next = temp.next.next;
-        return head;
-    }
+//    private static Node RemoveNthNodeFronEnd(Node head, int n){
+//        int cnt = 0;
+//        Node temp = head;
+//        //First Count the Size of the
+//        while (temp != null){
+//            cnt++;
+//            temp= temp.next;
+//        }
+//        //Edge case if the count == N then change the head
+//        if(cnt == n){
+//            Node newNode = head.next;
+//            return newNode;
+//        }
+//        int res = cnt - n;
+//        temp = head;
+//        while (temp != null){
+//           res--;
+//           if(res == 0) break;
+//            temp = temp.next;
+//        }
+//        temp.next = temp.next.next;
+//        return head;
+//    }
 
     // Better Using Slow and Fast Pointer
     private static Node RemoveNthNodeOptmize(Node head,int N){
